b7f8e2c4328f3491cf9a5874a17d0a42f85d576a
Cameron R <Contillex@gmail.com>
2018-11-03 10:10:37 +0000

    Added Functions to Load JS, CSS & Google Web Fonts.

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
-function custom_function() {
-    //code
+<?php
+function wpTheme1_scripts() {
+    wp_enqueue_style( 'boostrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
+    wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true);
 }
-add_action( 'action', 'custom_function');
+add_action( 'wp_enqueue_scripts', 'wpTheme1_scripts');
+
+function wpTheme1_google_fonts() {
+    wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+    wp_enqueue_style( 'OpenSans' );
+}
+add_action( 'wp_print_styles', 'wpTheme1_google_fonts');
+
+add_theme_support( 'title-tag' );

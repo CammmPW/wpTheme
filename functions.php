1e5ed7b5692b6f4cf4b61e70a6aca1ec4afe5516
Cameron R <Contillex@gmail.com>
2018-11-03 10:37:42 +0000

    Added Comments and Thumbnail Functionality.

--- functions.php
+++ functions.php
@@ -22,6 +22,7 @@ function custom_settings_add_menu() {
 }
 add_action('admin_menu', 'custom_settings_add_menu');
 
+
 //Custom Global Settings
 function custom_settings_page() { ?>
     <div class="wrap">
@@ -41,6 +42,7 @@ function setting_twitter() { ?>
     <input type="text" name="twitter" id="twitter" value="<?php echo get_option('twitter'); ?>" />
 <?php }
 
+//Github
 function setting_github() { ?>
     <input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
 <?php }
@@ -48,9 +50,13 @@ function setting_github() { ?>
 function custom_settings_page_setup() {
     add_settings_section('section', 'All Settings', null, 'theme-options' );
     add_settings_field('twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section');
-    add_settings_field('github', 'GitHub URL', 'setting_twitter', 'theme-options', 'section');
+    add_settings_field('github', 'GitHub URL', 'setting_github', 'theme-options', 'section');
 
     register_setting('section', 'twitter');
     register_setting('section', 'github');
 }
 add_action('admin_init', 'custom_settings_page_setup');
+
+
+//Support Thumbnail Images
+add_theme_support('post-thumbnails');
